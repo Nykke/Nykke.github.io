df0e6cecaaad8d0272476853d80a001be1df4dfd
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-04-11 19:06:38 -0400

    removed greensock, fixed script file

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-TweenMax.to(".moonman", 9, {x:1250, rotation: 360, scale: 0.5});
-TweenMax.staggerFrom(".spaceshuttle", 3, {y:-300, scale: 0.7, delay:9.5});
-
 
 var nav = function (transition){
 aElement.className='trans';
